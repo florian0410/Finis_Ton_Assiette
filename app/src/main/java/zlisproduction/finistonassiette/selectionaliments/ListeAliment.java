090d57d19bbe289be8e01117a7ae9e7a63a47a17
Thibaut <thibaut.lamblin@edu.esiee.fr>
2015-05-15 15:14:31 +0200

    listealiments

--- app/src/main/java/zlisproduction/finistonassiette/selectionaliments/ListeAliment.java
+++ app/src/main/java/zlisproduction/finistonassiette/selectionaliments/ListeAliment.java
@@ -1,9 +1,20 @@
 package zlisproduction.finistonassiette.selectionaliments;
 
+import java.util.ArrayList;
+
 /**
  * Created by Florian on 15/05/2015.
  */
 public class ListeAliment {
+
+
+    public ArrayList <Aliment> static alimentsArraylist (ArrayList<Aliment> parraylist)
+    {
+        int size=parraylist.size();
+        for (int i=0; i<size; i++){
+
+        }
+    }
 
 
 }
