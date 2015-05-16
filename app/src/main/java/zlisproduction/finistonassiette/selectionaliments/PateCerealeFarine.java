d0cbfff1ffcdb173cb34c6495bdaa67fa722d703
thibautLmn <thibaut.lamblin@edu.esiee.fr>
2015-05-16 15:17:32 +0200

    thibaut 16-05-2015 11 35

--- app/src/main/java/zlisproduction/finistonassiette/selectionaliments/PateCerealeFarine.java
+++
@@ -1,9 +0,0 @@
-package zlisproduction.finistonassiette.selectionaliments;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Florian on 15/05/2015.
- */
-public class PateRizCerealeFarine extends AppCompatActivity {
-}
