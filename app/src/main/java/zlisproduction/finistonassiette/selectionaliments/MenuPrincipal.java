c04e49ac063228499ac97068abdb70d5ecd84cb1
Florian <florian0410@hotmail.fr>
2015-05-22 21:12:29 +0200

    Integration menu en cours. Bug avec Fragment résultant d'un adapter

--- app/src/main/java/zlisproduction/finistonassiette/selectionaliments/MenuPrincipal.java
+++ app/src/main/java/zlisproduction/finistonassiette/selectionaliments/MenuPrincipal.java
@@ -1,12 +1,16 @@
 package zlisproduction.finistonassiette.selectionaliments;
 
-import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import java.util.ArrayList;
@@ -16,7 +20,7 @@ import zlisproduction.finistonassiette.R;
 /**
  * Created by Florian on 15/05/2015.
  */
-    public class MenuPrincipal extends Activity implements View.OnClickListener {
+    public class MenuPrincipal extends Fragment implements View.OnClickListener{
     /*
     déclaration des boutons
      */
@@ -25,75 +29,74 @@ import zlisproduction.finistonassiette.R;
     private Button viandePoissons;
     private Button produitsLaitiers;
     private Button fruitlegumes;
-
-
-
-
-
-
-
+    private Fragment fragment;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu_principal_layout);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       // FragmentActivity    faActivity  = (FragmentActivity)    super.getActivity();
+        // Replace LinearLayout by the type of the root element of the layout you're trying to load
+        View lLayout = inflater.inflate(R.layout.menu_principal_layout, container, false);
+        // Of course you will want to faActivity and llLayout in the class and not this method to access them in the rest of
+        // the class, just initialize them here
 
-
-        /*
+          /*
         Désérialisation des boutons et définition de l'écouteur
          */
-        incontournables=(Button)findViewById(R.id.incontournable);
+        incontournables=(Button) lLayout.findViewById(R.id.incontournable);
         incontournables.setOnClickListener(this);
 
-        patesFarinesCereales=(Button)findViewById(R.id.PatesFarinesCereales);
-        patesFarinesCereales.setOnClickListener(this);
+        patesFarinesCereales=(Button) lLayout.findViewById(R.id.PatesFarinesCereales);
+        // patesFarinesCereales.setOnClickListener(this);
 
-        viandePoissons=(Button)findViewById(R.id.viandePoissons);
-        viandePoissons.setOnClickListener(this);
+        viandePoissons=(Button) lLayout.findViewById(R.id.viandePoissons);
+        // viandePoissons.setOnClickListener(this);
 
-        produitsLaitiers=(Button)findViewById(R.id.produitsLaitiers);
-        produitsLaitiers.setOnClickListener(this);
+        produitsLaitiers=(Button) lLayout.findViewById(R.id.produitsLaitiers);
+        // produitsLaitiers.setOnClickListener(this);
 
-        fruitlegumes=(Button)findViewById(R.id.fruitlegumes);
-        fruitlegumes.setOnClickListener(this);
+        fruitlegumes=(Button) lLayout.findViewById(R.id.fruitlegumes);
+        // fruitlegumes.setOnClickListener(this);
 
-
+        // Instead of :
+        // findViewById(R.id.someGuiElement);
+        return lLayout; // We must return the loaded Layout
     }
+
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()){
+
+        switch (v.getId()) {
             case R.id.incontournable:
-                Intent activiteIncontournable= new Intent (MenuPrincipal.this,Incontournable.class);
-                startActivity(activiteIncontournable);
-
-
+                fragment = new Incontournable();
                 break;
+/*
             case R.id.PatesFarinesCereales:
-
-                Intent activitePatesFarineCereales= new Intent(MenuPrincipal.this,PatesFarinesCereales.class);
-                startActivity(activitePatesFarineCereales);
-
+                fragment = new PatesFarinesCereales();
                 break;
+
             case R.id.viandePoissons:
-
-                Intent activiteviandepoisson= new Intent(MenuPrincipal.this,ViandePoisson.class);
-                startActivity(activiteviandepoisson);
-
+                fragment = new ViandePoisson();
                 break;
+
             case R.id.produitsLaitiers:
+                fragment = new ProduitLaitier();
 
-                Intent activiteProduitsLaitiers = new Intent(MenuPrincipal.this,ProduitLaitier.class);
-                startActivity(activiteProduitsLaitiers);
 
-                break;
             case R.id.fruitlegumes:
-                Intent activiteFruitsLegumes= new Intent (MenuPrincipal.this,FruitLegume.class);
-                startActivity(activiteFruitsLegumes);
-
-                break;
+               fragment = new FruitLegume();
+                break;*/
         }
+        if (fragment != null) {
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_container, fragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+        else {
+            // error in creating fragment
+            Log.e("MainActivity", "Error in creating fragment");
+        }
+
     }
-
-
 }
