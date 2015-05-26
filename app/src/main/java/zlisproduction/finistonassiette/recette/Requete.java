ece0a4705fb87de6481edb83780bac5adf6a8fde
thibautLmn <thibaut.lamblin@edu.esiee.fr>
2015-05-26 10:44:00 +0200

    thibaut 16-05-2015 11 35

--- app/src/main/java/zlisproduction/finistonassiette/recette/Requete.java
+++ app/src/main/java/zlisproduction/finistonassiette/recette/Requete.java
@@ -1,11 +1,16 @@
 package zlisproduction.finistonassiette.recette;
 
+import android.os.AsyncTask;
+import android.widget.ProgressBar;
+
 import java.util.ArrayList;
+
+import zlisproduction.finistonassiette.selectionaliments.Result;
 
 /**
  * Created by Thibaut on 24/05/2015.
  */
-public interface Requete {
+public interface Requete  {
 
     public void envoyerRequete(ArrayList pData);
 }
