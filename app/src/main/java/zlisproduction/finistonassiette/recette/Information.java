286ee2aefe485882e3cb0c98901741715f9674f0
thibautLmn <thibaut.lamblin@edu.esiee.fr>
2015-05-26 11:08:50 +0200

    information.java

---
+++ app/src/main/java/zlisproduction/finistonassiette/recette/Information.java
@@ -0,0 +1,75 @@
+package zlisproduction.finistonassiette.recette;
+import android.app.Activity;
+
+//test
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Thibaut on 24/05/2015.
+ */
+public class Information extends Activity {
+
+
+    //attribut par défaut
+    Instanciation instancie=null;
+    ConsulterRecette requete =null;
+    AnalyseResultat analyse_resultat=null;
+
+
+    //test attribut
+    ArrayList<String> array;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        instancie= new ConstructeurDefaut();
+        requete = new ConsulterRecette(Information.this);
+        analyse_resultat = new JsonFormat();
+
+        array= new ArrayList<String>();
+        array.add("lol");
+        requete.execute(array);
+
+
+    }
+
+
+    public void envoyerRequete(ArrayList pArray){
+        this.requete.envoyerRequete(pArray);
+    }
+
+    public void analyserResultat(){
+        this.analyse_resultat.parseData();
+    }
+
+    public void instanciationRecette(){
+        this.instancie.instancie();
+    }
+
+    public Instanciation getInstancie() {
+        return instancie;
+    }
+
+    public void setInstancie(Instanciation instancie) {
+        this.instancie = instancie;
+    }
+
+    public Requete getRequete() {
+        return requete;
+    }
+
+    //public void setRequete(Requete requete) {
+    //this.requete = requete;
+    //}
+
+    public AnalyseResultat getAnalyse_resultat() {
+        return analyse_resultat;
+    }
+
+    public void setAnalyse_resultat(AnalyseResultat analyse_resultat) {
+        this.analyse_resultat = analyse_resultat;
+    }
+}
