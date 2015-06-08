8526f8f333a598a252022b882a4cae8a0f6b3320
Florian.S <florian0410@hotmail.fr>
2015-06-08 09:38:40 +0200

    Ajout classe ProductDisplayer

---
+++ app/src/main/java/zlisproduction/finistonassiette/selectionaliments/barcodescanner/ProductResultDisplayer.java
@@ -0,0 +1,14 @@
+package zlisproduction.finistonassiette.selectionaliments.barcodescanner;
+
+import android.app.Fragment;
+
+/**
+ * Created by Florian.S on 05/06/2015.
+ * Cette classe sera affichée sur une même page que le produit scanné par l'utilisateur lors de l'utilisation du scanner
+ */
+public class ProductResultDisplayer extends Fragment {
+
+    public void getCategoriesFromJSON(){
+
+    }
+}
